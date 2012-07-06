254d998636606438d1775e254f684a92df421b00
Alexis Bernard <alexis@official.fm>
2012-07-06 16:44:50 +0200

    Add few more samples.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 %w(
+  http://new.official.fm/tracks/2d8q
+  http://new.official.fm/tracks/c9Zd
+  http://new.official.fm/tracks/2g1w
   http://new.official.fm/playlists/1rp7
   http://new.official.fm/tracks/SuGB
   http://new.official.fm/tracks/7W5f
