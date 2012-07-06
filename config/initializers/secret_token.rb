a540e43daa169785947da69fbf33042a1b19805f
Alexis Bernard <alexis@official.fm>
2012-07-06 15:01:26 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwagFm::Application.config.secret_token = '00a00908a3e9626c3e957e543d511f1dc576b015978134b92f7f856e0214c83ce0efb5c93e371883346af9b5285b6147d1166b0f0c13ab48dd6bf17d3dd84811'
