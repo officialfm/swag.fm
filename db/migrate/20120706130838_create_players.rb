9f4bbaba3371f5036eed7507bdc2530affb6fe08
Alexis Bernard <alexis@official.fm>
2012-07-13 14:17:12 +0200

    Stuff.

--- db/migrate/20120706130838_create_players.rb
+++ db/migrate/20120706130838_create_players.rb
@@ -2,7 +2,8 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :url, null: false
-
+      t.string :steam_url, null: false
+      t.string :picture_url, null: false
       t.timestamps
     end
   end
