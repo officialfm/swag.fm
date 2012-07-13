0834f1f5c5c6a34fcb0ebcd5feb2d766be78ff46
Alexis Bernard <alexis@official.fm>
2012-07-13 14:45:06 +0200

    Remouve unused fields.

--- db/migrate/20120706130838_create_players.rb
+++ db/migrate/20120706130838_create_players.rb
@@ -2,8 +2,6 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :url, null: false
-      t.string :steam_url, null: false
-      t.string :picture_url, null: false
       t.timestamps
     end
   end
