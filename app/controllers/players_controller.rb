42f37bcb04955318a1fd8fb38fe3cb18f47376b5
Vincent Pochet <vincent@official.fm>
2012-07-20 15:16:25 +0200

    Auto update chrome extension Check for boormarked status of a page Update bookmarked icon Set extension version to 0.3

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,10 +5,18 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(params[:player])
     if @player.save
-      redirect_to '/'
+      respond_to do |format|
+        format.html { redirect_to '/' }
+        format.json { render json: @player }
+      end
     else
       render @player.errors, status: :unprocessable_entity
     end
+  end
+
+  def bookmarks
+    @players = Player.find_by_url(params[:url])
+    render json: @players
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SwagFm::Application.routes.draw do
 
-  resources :players, only: %w[create]
+  resources :players, only: %w[create] do
+    get :bookmarks, on: :collection
+  end
 
   resources :extensions, only: [] do
     get :chrome, on: :collection
