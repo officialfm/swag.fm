218e9a39b3181aa42d379fd9f38a5956ae114d4b
Vincent Pochet <vincent@official.fm>
2012-07-20 16:43:24 +0200

    Reload swag.fm after action on bookmark

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 %w(
-  http://new.official.fm/tracks/2d8q
-  http://new.official.fm/tracks/c9Zd
-  http://new.official.fm/tracks/2g1w
-  http://new.official.fm/playlists/1rp7
-  http://new.official.fm/tracks/SuGB
-  http://new.official.fm/tracks/7W5f
-  http://new.official.fm/tracks/LxOu
-  http://new.official.fm/tracks/9kAi
-  http://new.official.fm/tracks/Gmdl
-  http://new.official.fm/tracks/8DGT
-  http://new.official.fm/tracks/O357
+  http://official.fm/tracks/2d8q
+  http://official.fm/tracks/c9Zd
+  http://official.fm/tracks/2g1w
+  http://official.fm/playlists/1rp7
+  http://official.fm/tracks/SuGB
+  http://official.fm/tracks/7W5f
+  http://official.fm/tracks/LxOu
+  http://official.fm/tracks/9kAi
+  http://official.fm/tracks/Gmdl
+  http://official.fm/tracks/8DGT
+  http://official.fm/tracks/O357
 ).each { |url| Player.create!(url: url) }
