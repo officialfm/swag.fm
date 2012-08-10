0ad4737d19a9da2d69b8cc96fd8fd660f7723ed1
Alexis Bernard <alexis@official.fm>
2012-08-10 17:29:12 +0200

    Fix test.

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionController::TestCase
-  def test_index
-    get(:index)
+  def test_show
+    get(:show)
     assert_response(:success)
   end
 
   def test_create_on_success
-    post(:create, email: 'alice@example.com', password: 'password')
+    post(:create, session: {email: 'alice@example.com', password: 'password'})
     assert_equal(users(:alice).id, session[:user_id])
     assert_response(:redirect)
   end
 
   def test_create_on_failure
-    post(:create, email: 'doesnt@exist.com', password: 'password')
-    assert_template('session/index')
+    post(:create, session: {email: 'doesnt@exist.com', password: 'password'})
+    assert_template('session/show')
     assert_response(:success)
   end
 end
