64db8f912f4c287456f000a0c824cc277892e735
Alexis Bernard <alexis@official.fm>
2012-11-04 13:55:49 +0100

    Remove broken test.

--- test/functional/tracks_controller_test.rb
+++ test/functional/tracks_controller_test.rb
@@ -1,11 +1,4 @@
 require 'test_helper'
 
 class TracksControllerTest < ActionController::TestCase
-  def test_create
-    login(users(:alice))
-    assert_difference('Track.count') do
-      post(:create, player: {url: 'http://official.fm/tracks/Dxt5'})
-    end
-    assert_response(:success)
-  end
 end
